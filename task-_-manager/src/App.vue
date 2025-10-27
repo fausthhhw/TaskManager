45c3bbd9ec4ef6cc28cfe921137b0995ffa8d1cb
fausthhhw <sobaka8058@gmail.com>
2025-10-27 23:10:11 +0300

    Сделал ДЗ

---
+++ task-_-manager/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+<h1>Каталог задач</h1>
+<div class="tasks">
+<TaskCard
+v-for="task in tasks"
+:Task="task" />
+
+</div>
+</div>
+</template>
+<script setup lang="ts">
+import TaskCard from "./components/TaskCard.vue";
+const tasks = [
+  {id: "1", title: "Погладить кошку", text: "Требуется тщательно погладить кошку. Для выполнения задачи нужна рука (опционально)."},
+  {id: "2", title: "Сварить пельмени", text: "Нужно положить в кипящую воду с приправой пятьдесят шесть пельменей."},
+  {id: "3", title: "Сыграть в Counter-Strike 2", text: "Включить компьютер, запустить Steam и игру."},
+  {id: "4", title: "Купить лимонную воду", text: "Взять со стола сто рублей, сходить в магазин."},
+  {id: "5", title: "Выполнить домашние задания", text: "Просмотреть беседу учебной группы, взвесить все за и против, лечь спать."},
+]
+</script>
+<style>
+.tasks {
+display: flex;
+gap: 16px;
+flex-direction: column;
+}
+</style>
---
+++ task-_-manager/src/components/TaskCard.vue
@@ -0,0 +1,29 @@
+<template>
+<div class="Task-Card">
+<main>
+<div>{{ props.Task.title }}</div>
+<div>{{ props.Task.text }}</div>
+</main>
+<footer>
+<button type="button" @click="CompleteTask">Выполнить задачу</button>
+</footer>
+</div>
+</template>
+<script setup lang="ts">
+const props = defineProps<{
+Task: { title: string; text: string};
+}>();
+function CompleteTask() {
+alert(`Поздравляем, вы выполнили задание: ${props.Task.title}`);
+}
+</script>
+<style>
+.Task-Card {
+display: flex;
+gap: 4px;
+flex-direction: column;
+border: 1px solid #ccc;
+padding: 12px;
+border-radius: 8px;
+}
+</style>
